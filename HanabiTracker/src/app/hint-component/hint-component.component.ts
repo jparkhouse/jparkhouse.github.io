5bb59521f69a855d16b8b252818210e05909fff0
jparkhouse <jparkhouse1511@gmail.com>
2023-10-28 12:02:01 +0100

    Generated services and components

---
+++ HanabiTracker/src/app/hint-component/hint-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hint-component',
+  templateUrl: './hint-component.component.html',
+  styleUrls: ['./hint-component.component.css']
+})
+export class HintComponentComponent {
+
+}
